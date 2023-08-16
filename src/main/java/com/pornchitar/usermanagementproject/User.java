6f068c06dbfe55409c3507eea17ab70e3733aa09
Pornchitar Sarsai <kream21574@gmail.com>
2023-08-17 05:47:18 +0700

    User

--- src/main/java/com/pornchitar/usermanagementproject/User.java
+++ src/main/java/com/pornchitar/usermanagementproject/User.java
@@ -4,17 +4,24 @@
  */
 package com.pornchitar.usermanagementproject;
 
+import java.io.Serializable;
+
 /**
  *
  * @author ASUS
  */
-public class User {
+public class User implements Serializable{
     private int id;
     private String login;
     private String name;
     private String password;
     private char gender;
     private char role;
+    private static int lastID = 1;
+
+    public User(String login, String name, String password, char gender, char role) {
+        this(User.lastID++, login, name, password, gender, role);
+    }
 
     public User(int id,String login, String name, String password, char gender, char role) {
         this.id = id;
@@ -24,5 +31,67 @@ public class User {
         this.gender = gender;
         this.role = role;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public char getRole() {
+        return role;
+    }
+
+    public void setRole(char role) {
+        this.role = role;
+    }
+
+    public static int getLastID() {
+        return lastID;
+    }
+
+    public static void setLastID(int lastID) {
+        User.lastID = lastID;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", login=" + login + ", name=" + name + ", password=" + password + ", gender=" + gender + ", role=" + role + '}';
+    }
+    
     
 }
